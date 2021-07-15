be339642ddb17aae53e172bbf415fb62f8b75122
Ray Addo <addoray40@gmail.com>
2021-07-15 19:19:33 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+ int main()
+{
+    int A,B, sum;
+    cout<<"Enter first integer : ";
+    cin>>A;
+    cout<<"Enter second integer : ";
+    cin>>B;
+    sum = A + B;
+    cout<<A<<" + "<<B<<" = "<<sum;
+    return 0;
+}
